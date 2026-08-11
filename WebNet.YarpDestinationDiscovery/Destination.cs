53ff22959358ba626b609be386bbe9070b632a6e
JoJo Webster <151088710+miserix13@users.noreply.github.com>
2026-08-11 09:21:56 -0500

    Add project files.

---
+++ WebNet.YarpDestinationDiscovery/Destination.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace WebNet.YarpDestinationDiscovery
+{
+    public record Destination
+    {
+        public string Address { get; set; }
+        public string Health { get; set; }
+        public string Host { get; set; }
+        public Dictionary<string, string> Metadata { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Destination>(this);
+        }
+
+        public static Destination Parse(string value)
+        {
+            return JsonSerializer.Deserialize<Destination>(value);
+        }
+
+        public Yarp.ReverseProxy.Configuration.DestinationConfig GetDestinationConfig()
+        {
+            return new() { Address = this.Address, Health = this.Health, Host = this.Host, Metadata = this.Metadata };
+        }
+
+        public static Destination GetDestination(Yarp.ReverseProxy.Configuration.DestinationConfig config)
+        {
+            return new() { Address = config.Address, Health = config.Health, Host = config.Host, Metadata = config.Metadata.ToDictionary() };
+        }
+    }
+}
---
+++ WebNet.YarpDestinationDiscovery/IDestinationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WebNet.YarpDestinationDiscovery
+{
+    public interface IDestinationProvider<TKey>
+        where TKey : struct, IEquatable<TKey>
+    {
+        Task<ReadOnlyDictionary<TKey, Destination>> GetAllAsync();
+        Task<Destination> GetDestinationAsync(TKey key);
+        Task RegisterAsync(TKey key, Destination value);
+        Task UnregisterAsync(TKey key);
+    }
+}
